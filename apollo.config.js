e28ba7ac77d117780b0ae53797f4d867d56a8c65
matt <matt.pocock@thevirtualforge.com>
2018-12-28 22:34:45 +0000

    Changed API endpoint

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,10 @@ module.exports = {
   client: {
     service: {
       name: 'pronunciation-service',
-      endpoint: 'http://localhost:4001/graphql',
+      endpoint:
+        process.env.NODE_ENV === 'development'
+          ? 'http://localhost:4001/graphql'
+          : 'https://owk36r3y7g.execute-api.us-east-1.amazonaws.com/dev/graphql',
     },
   },
 };
